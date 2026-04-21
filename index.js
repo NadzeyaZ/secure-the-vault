81cef6156e564298e46b7aee11bba178cd7d4edd
NadzeyaSam <nadzeya@videa.ai>
2026-04-20 21:48:24 -0400

    code secured

---
+++ index.js
@@ -0,0 +1,8 @@
+let code1;
+code1 = 7 + 3;
+let code2 = 100 / 2 - 10;
+let code3 = (15 % 4) + 36;
+let message = "The vault has been secured. The combination is: ";
+let codeA = code1 + "-" + code2 + "-" + code3;
+let codeB = `${code1}-${code2}-${code3}`;
+console.log(message, codeA, codeB);
